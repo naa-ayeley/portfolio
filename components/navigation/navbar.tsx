11091a9578e74812982cdc1110e33b4d80fe45a6
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-09-15 12:09:00 +0000

    completed navbar

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -5,19 +5,35 @@ import Image from 'next/image';
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { getInitials } from '@/lib/utils';
+import { OWNER } from '@/lib/constants';
+// eslint-disable-next-line 
+import { FiMenu, FiX } from 'react-icons/fi'
+import { useState } from 'react';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/accordion';
 
 export default function Navbar() {
+    const [showMenu, setShowMenu] = useState(false)
     const pathname = usePathname()
-    console.log(pathname);
 
-    const isActive = (href: string) => {
+    const closeMenu = (): void => {
+        setShowMenu(false)
+    }
+
+    const openMenu = (): void => {
+        setShowMenu(true)
+    }
+
+
+    const isActive = (href: string): boolean => {
         return pathname === href || pathname.startsWith(href)
     }
 
     return (
         <section className='w-full'>
             {/* Desktop */}
-            <header className="p-4 flex items-center gap-8 justify-between ">
+            <header className="p-4 md:flex hidden items-center gap-8 justify-between ">
                 <Link href='/'>
                     <Image src="/images/logos/logo.svg" alt="Logo" width={190} height={40} />
                 </Link>
@@ -29,7 +45,7 @@ export default function Navbar() {
                                     nav.sublinks ? (
                                         <Popover>
                                             <PopoverTrigger asChild>
-                                                <button className={`${!isActive(nav.href) && "bg-bluey/20 font-semibold px-6 py-1 rounded-sm"}`} >{nav.name}</button>
+                                                <button className={`${isActive(nav.href) && "bg-bluey/20 font-semibold px-6 py-1 rounded-sm"}`} >{nav.name}</button>
                                             </PopoverTrigger>
                                             <PopoverContent className='w-max flex flex-col p-2'>
                                                 {
@@ -40,7 +56,7 @@ export default function Navbar() {
                                             </PopoverContent>
                                         </Popover>
                                     ) : (
-                                        <Link href={nav.href}>
+                                        <Link className={`${isActive(nav.href) && "bg-bluey/20 font-semibold px-6 py-1 rounded-sm"}`} href={nav.href}>
                                             {nav.name}
                                         </Link>
                                     )
@@ -51,11 +67,72 @@ export default function Navbar() {
                 </nav>
             </header>
             {/* Mobile */}
+            <header className='w-full  flex md:hidden items-center justify-between py-4'>
+                <Link href='/'>
+                    <Avatar className='w-[50px] h-[50px]'>
+                        <AvatarImage src="/images/logos/favicon.svg" />
+                        <AvatarFallback> {getInitials(OWNER)} </AvatarFallback>
+                    </Avatar>
+                </Link>
+
+                <button className='text-3xl relative z-10' onClick={openMenu}>
+                    <FiMenu />
+                </button>
+
+                <div className={`slide-menu hide-scroll bg-muted flex flex-col gap-8 items-center justify-center relative ${showMenu ? 'show' : 'hide'} `}>
+                    <button className=' text-3xl absolute right-4  top-8 z-50  ' onClick={closeMenu}>
+                        <FiX />
+                    </button>
+                    <nav className='flex w-full flex-col mt-4 gap-8 px-4'>
+                        <Link href='/' className='mx-auto' onClick={closeMenu}>
+                            <Image src="/images/logos/logo.svg" alt="Logo" width={230} height={40} />
+                        </Link>
+
+                        <ul className='flex flex-col gap-4 w-full relative'>
+                            {
+                                navlinks.map((nav) => (
+                                    <li className=' w-full' key={nav.href}>
+                                        {
+                                            nav.sublinks ? (
+                                                <Accordion type="single" collapsible className="w-full px-4 decoration-transparent">
+                                                    <AccordionItem value="item-1">
+                                                        <AccordionTrigger className='montserrat'>{nav.name}</AccordionTrigger>
+                                                        <AccordionContent className=''>
+                                                            <div className='flex flex-col gap-2'>
+
+                                                                {
+                                                                    nav.sublinks.map((sublink) => (
+                                                                        <Sublink key={sublink.href} {...sublink} />
+                                                                    ))
+                                                                }
+                                                            </div>
+
+                                                        </AccordionContent>
+                                                    </AccordionItem>
+                                                </Accordion>
+                                            ) : (
+                                                <div onClick={closeMenu} className={`py-2 px-4 ${isActive(nav.href) && "bg-bluey/20 font-semibold  w-full rounded-sm"}`} >
+                                                    <Link href={nav.href}>
+                                                        {nav.name}
+                                                    </Link>
+                                                </div>
+                                            )
+                                        }
+                                    </li>
+                                ))
+                            }
+
+                        </ul>
+                    </nav>
+                </div>
+
+            </header>
         </section>
     )
 }
 
-const Sublink = ({ name, href } : Navlink) => {
+const Sublink = ({ name, href }: Navlink) => {
+    
     return (
         <Link className='hover:bg-bluey/20 text-bluey px-4 rounded-lg py-2' href={href}>
             {name}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
+/* eslint-disable */
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getInitials(name: string) {
+  const split = name.toUpperCase().split(" ")
+  if (split.length === 1) return split[0][0]
+  const first = split[0]
+  const last = split[split.length - 1]
+  return first[0] + last[0]
 }
