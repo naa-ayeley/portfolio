290bcb908d337318848e365e2d6d371c6738129e
Nutifafa Afi Attor <attorfafa@gmail.com>
2024-03-21 05:49:10 +0000

    removed sublinks for work

--- lib/data.tsx
+++ lib/data.tsx
@@ -39,24 +39,6 @@ export const navlinks: Navlink[] = [
     {
         name: "Work",
         href: "/work",
-        sublinks: [
-            {
-                name: "Web Designs",
-                href: "/work#web-designs"
-            },
-            {
-                name: "Mobile Designs",
-                href: "/work#web-designs"
-            },
-            {
-                name: "Product Designs",
-                href: "/work#product-designs"
-            },
-            {
-                name: "Case Studies",
-                href: "/work#case-studies"
-            },
-        ]
     },
     {
         name: "Resume",
